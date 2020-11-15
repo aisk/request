c8abe39839d665745acd6adb6afb5a1795564db3
An Long <aisk1988@gmail.com>
2020-11-15 23:58:26 +0800

    ✨ support basic sending requests

--- src/Network/HTTP/Request.hs
+++ src/Network/HTTP/Request.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.HTTP.Request
   ( Request
   , Response
@@ -5,7 +7,14 @@ module Network.HTTP.Request
   , Headers
   ) where
 
-import qualified Data.ByteString as BS
+import qualified Data.ByteString           as BS
+import qualified Data.ByteString.Char8     as C
+import qualified Data.ByteString.Lazy      as LBS
+import qualified Data.CaseInsensitive      as CI
+import qualified Data.List                 as List
+import qualified Network.HTTP.Client       as LowLevelClient
+import qualified Network.HTTP.Client.TLS   as LowLevelTLSClient
+import qualified Network.HTTP.Types.Status as LowLevelStatus
 
 type Header = (BS.ByteString, BS.ByteString)
 
@@ -29,8 +38,38 @@ data Request = Request
   , requestBody    :: Maybe BS.ByteString
   } deriving (Show)
 
+toLowlevelRequest :: Request -> IO LowLevelClient.Request
+toLowlevelRequest (req) = do
+  initReq <- LowLevelClient.parseRequest $ requestUrl req
+  return $ initReq { LowLevelClient.method = C.pack . show $ requestMethod req
+                   , LowLevelClient.requestHeaders = map (\(k, v) -> (CI.mk k, v)) $ requestHeaders req
+                   }
+
 data Response = Response
   { responseStatus  :: Int
   , responseHeaders :: Headers
   , responseBody    :: BS.ByteString
   } deriving (Show)
+
+fromLowLevelRequest :: LowLevelClient.Response LBS.ByteString -> Response
+fromLowLevelRequest (res) =
+  let status = LowLevelStatus.statusCode . LowLevelClient.responseStatus $ res
+      body = LBS.toStrict $ LowLevelClient.responseBody res
+      headers = LowLevelClient.responseHeaders res
+  in
+  Response status (map (\(k,v) ->
+                         let hk = CI.original k
+                         in
+                         (hk, v)) headers) body
+
+getManagerForUrl :: String -> IO LowLevelClient.Manager
+getManagerForUrl url =
+    if (List.isPrefixOf "https" url) then LowLevelClient.newManager LowLevelTLSClient.tlsManagerSettings
+                                     else LowLevelClient.newManager LowLevelClient.defaultManagerSettings
+
+send :: Request -> IO Response
+send (req) = do
+  manager <- getManagerForUrl $ requestUrl req
+  llreq <- toLowlevelRequest req
+  llres <- LowLevelClient.httpLbs llreq manager
+  return $ fromLowLevelRequest llres
