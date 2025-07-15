fc486828f401cee67f98fb5c8846e40e91efd3b5
AN Long <aisk1988@gmail.com>
2025-07-15 23:04:37 +0900

    ✅ Add basic test

---
+++ test/Spec.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Test.Hspec
+import Network.HTTP.Request
+
+
+main :: IO ()
+main = hspec $ do
+  describe "Network.HTTP.Request" $ do
+    it "should fetch example.com and return 200 OK" $ do
+      response <- get "http://example.com"
+      responseStatus response `shouldBe` 200
+
+    it "should send a request to example.com and return 200 OK" $ do
+      response <- send (Request GET "http://example.com" [] Nothing)
+      responseStatus response `shouldBe` 200
+
+    it "should post to httpbin.org/post and return 200 OK" $ do
+      response <- post ("https://postman-echo.com/post", Just "Hello!")
+      responseStatus response `shouldBe` 200
+
+    it "should put to httpbin.org/put and return 200 OK" $ do
+      response <- put ("https://postman-echo.com/put", Just "Hello!")
+      responseStatus response `shouldBe` 200
