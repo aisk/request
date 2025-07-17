166092d9bead7afff418d92e7a2f8b52c69031c9
AN Long <aisk1988@gmail.com>
2025-07-17 23:19:43 +0900

    ✅ Refactor test

--- test/Spec.hs
+++ test/Spec.hs
@@ -31,19 +31,19 @@ main = hspec $ do
       response <- patch ("https://postman-echo.com/patch", Just "Hello!")
       responseStatus response `shouldBe` 200
 
-  it "should use dot record syntax to create and access request/response" $ do
-    let req = Request { method = GET, url = "http://example.com", headers = [("User-Agent", "Haskell-Request")], body = Nothing }
-    response <- send req
-    response.status `shouldBe` 200
-    response.headers `shouldSatisfy` (not . null)
+    it "should use dot record syntax to create and access request/response" $ do
+      let req = Request { method = GET, url = "http://example.com", headers = [("User-Agent", "Haskell-Request")], body = Nothing }
+      response <- send req
+      response.status `shouldBe` 200
+      response.headers `shouldSatisfy` (not . null)
 
-  it "should access response body with different IsString types" $ do
-    -- Test with ByteString body
-    let req1 = Request { method = GET, url = "http://example.com", headers = [], body = Nothing }
-    response1 <- send req1 :: IO (Response BS.ByteString)
-    BS.length response1.body `shouldSatisfy` (> 0)
+    it "should access response body with different IsString types" $ do
+      -- Test with ByteString body
+      let req1 = Request { method = GET, url = "http://example.com", headers = [], body = Nothing }
+      response1 <- send req1 :: IO (Response BS.ByteString)
+      BS.length response1.body `shouldSatisfy` (> 0)
 
-    -- Test with String body
-    let req2 = Request { method = GET, url = "http://example.com", headers = [], body = Nothing }
-    response2 <- send req2 :: IO (Response String)
-    not (null response2.body) `shouldBe` True
+      -- Test with String body
+      let req2 = Request { method = GET, url = "http://example.com", headers = [], body = Nothing }
+      response2 <- send req2 :: IO (Response String)
+      not (null response2.body) `shouldBe` True
