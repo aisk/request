a5324d79dcd3abee30524f89c0c961cafda76f4d
AN Long <aisk1988@gmail.com>
2026-02-08 13:51:03 +0900

    ♻️ Replace IsString with FromResponseBody

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,32 +15,32 @@ main :: IO ()
 main = hspec $ do
   describe "Network.HTTP.Request" $ do
     it "should fetch example.com and return 200 OK" $ do
-      response <- get "http://example.com"
+      response <- get "http://example.com" :: IO (Response String)
       responseStatus response `shouldBe` 200
 
     it "should send a request to example.com and return 200 OK" $ do
-      response <- send (Request GET "http://example.com" [] Nothing)
+      response <- send (Request GET "http://example.com" [] Nothing) :: IO (Response String)
       responseStatus response `shouldBe` 200
 
     it "should post to httpbin.org/post and return 200 OK" $ do
-      response <- post "https://postman-echo.com/post" (Just "Hello!")
+      response <- post "https://postman-echo.com/post" (Just "Hello!") :: IO (Response String)
       responseStatus response `shouldBe` 200
 
     it "should put to httpbin.org/put and return 200 OK" $ do
-      response <- put "https://postman-echo.com/put" (Just "Hello!")
+      response <- put "https://postman-echo.com/put" (Just "Hello!") :: IO (Response String)
       responseStatus response `shouldBe` 200
 
     it "should patch to httpbin.org/patch and return 200 OK" $ do
-      response <- patch "https://postman-echo.com/patch" (Just "Hello!")
+      response <- patch "https://postman-echo.com/patch" (Just "Hello!") :: IO (Response String)
       responseStatus response `shouldBe` 200
 
     it "should use dot record syntax to create and access request/response" $ do
       let req = Request { method = GET, url = "http://example.com", headers = [("User-Agent", "Haskell-Request")], body = Nothing }
-      response <- send req
+      response <- send req :: IO (Response String)
       response.status `shouldBe` 200
       response.headers `shouldSatisfy` (not . null)
 
-    it "should access response body with different IsString types" $ do
+    it "should access response body with different types" $ do
       -- Test with ByteString body
       let req1 = Request { method = GET, url = "http://example.com", headers = [], body = Nothing }
       response1 <- send req1 :: IO (Response BS.ByteString)
